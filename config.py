582296141807120c52d686bfb78888542d6acf42
VipFitnessBot <19dib79@gmail.com>
2025-08-24 17:12:30 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
 # Опціонально: можна задати ці значення через Railway Variables замість редагування файлу.
 
-BOT_TOKEN      = ""  # або залиш порожнім і створи файл token.txt
-WFP_MERCHANT   = ""  # merchantAccount з WayForPay
-WFP_SECRET     = ""  # SecretKey з WayForPay
-PUBLIC_URL     = ""  # https://<app>.up.railway.app
+BOT_TOKEN      = "7717901847:AAHytaN_hObl-6G8IB43r8qhRSZ7svnO6gM"  # або залиш порожнім і створи файл token.txt
+WFP_MERCHANT   = "www_instagram_com_84d14"  # merchantAccount з WayForPay
+WFP_SECRET     = "23434a4fff7cd0e1b1b2f928ffa41d40370bc4b1"  # SecretKey з WayForPay
+PUBLIC_URL     = "https://web-production-ee6a9.up.railway.app"  # https://<app>.up.railway.app
 BOT_USERNAME   = ""  # юзернейм бота без @
 WFP_RETURN_URL = ""  # наприклад https://t.me/<твій_бот>
 SUBSCRIPTION_AMOUNT = 100
